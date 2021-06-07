791ac4f52d6477719e25c8c9037c991e6b9f1c67
Ed <65623961+efong505@users.noreply.github.com>
2021-06-06 18:14:11 -0600

    Update pyinstall.bat

--- pyinstall.bat
+++ pyinstall.bat
@@ -5,3 +5,6 @@ pip install pygame
 pip install pillow
 pip install pyinstaller
 pyinstaller --windowed --icon=dice.ico FongFinalRevised.py
+copy splash.png .\dist\FongFinalRevised\
+copy game-over2.jpg .\dist\FongFinalRevised\
+copy youwon.jpg .\dist\FongFinalRevised\
