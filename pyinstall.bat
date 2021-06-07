1c6c3462b4d66131041afd511a41037a260d2c8e
Ed <65623961+efong505@users.noreply.github.com>
2021-06-07 09:52:56 -0600

    Update pyinstall.bat

--- pyinstall.bat
+++ pyinstall.bat
@@ -9,3 +9,5 @@ copy splash.png .\dist\FongFinalRevised\
 copy game-over2.jpg .\dist\FongFinalRevised\
 copy youwon.jpg .\dist\FongFinalRevised\
 copy dice.ico .\dist\FongFinalRevised\
+copy ooh.mp3 .\dist\FongFinalRevised\
+copy applause.wav .\dist\FongFinalRevised\
